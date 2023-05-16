205545e3c9e6055b1d057a21f5fb85102de3264b
suraj kannujiya <82014093+Suraj-1233@users.noreply.github.com>
2023-05-16 09:00:09 +0530

    this

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+import {Route,Routes} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#1313a] min-h-screen flex flex-row">
+       hfhhghf
+
+    </div>
+    
+  );
+}
